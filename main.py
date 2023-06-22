26ec6e216e787a8333434dd99675ea42962e8a62
there1snog0d <pb_4game10@mail.ru>
2023-06-22 22:04:24 +0700

    обновил

--- main.py
+++ main.py
@@ -54,6 +54,12 @@ class New_tab():
         self.initialize_user_interface(c1, c2, c3, fname)
         self.insert_data(fname)
 
+        try:
+            f = open('Temp/pincode.txt', 'r')
+            f.close()
+        except:
+            self.create_pincode()
+
     def initialize_user_interface(self, c1, c2, c3, fname):
         self.f_top = ttk.Frame(self.tab)
         self.f_bot = ttk.Frame(self.tab)
@@ -74,13 +80,78 @@ class New_tab():
             self.new_list.column(col, anchor='center')
             self.new_list.heading(col, text=col.capitalize())
 
-        self.list=self.new_list
+        self.list = self.new_list
+        self.list.bind("<Double-1>", lambda x: self.OnDoubleClick(fname))
+
+    def commit_pincode(self,pin):
+        f = open('Temp/pincode.txt', "w+")
+        f.write(self.new_key.encrypt_func(pin))
+        f.close()
+
+    def create_pincode(self):
+        create_window = tk.Tk()
+        create_window.title("Придумайте пин-код")
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
+
+        create_frame = tk.Frame(create_window)
+        create_frame .pack(expand=True)
+
+        pincode_entry = tk.Entry(create_frame)
+        pincode_entry.grid(row=0, column=1)
+
+        button1 = tk.Button(create_frame, text="Готово",
+                            command=lambda: [self.commit_pincode(pincode_entry.get()), create_window.destroy() ])
+        button1.grid(row=1, column=1)
+
+        window_width = 250
+        window_height = 250
+        x = (screen_width // 2) - (window_width // 2)
+        y = (screen_height // 2) - (window_height // 2)
+        create_window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+        create_window.resizable(width=False, height=False)
+
+    def OnDoubleClick(self, fname):
+
+        check_window = tk.Tk()
+        check_window.title("Введите пин-код")
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
+
+        check_frame = tk.Frame(check_window)
+        check_frame.pack(expand=True)
+
+        pincode_entry = tk.Entry(check_frame)
+        pincode_entry.grid(row=0, column=1)
+
+        button1 = tk.Button(check_frame, text="Готово",
+                                         command=lambda: [self.check_func(pincode_entry, fname), check_window.destroy()])
+        button1.grid(row=1, column=1)
+
+
+        window_width = 250
+        window_height = 250
+        x = (screen_width // 2) - (window_width // 2)
+        y = (screen_height // 2) - (window_height // 2)
+        check_window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+        check_window.resizable(width=False, height=False)
+
+    def check_func(self, entry, fname):
+        pincode = ''
+        with open('Temp/pincode.txt', 'r') as f: pincode = f.read()
+        pincode = self.new_key.decrypt_func(pincode)
+
+        if pincode == entry.get():
+            self.show_info(fname)
+        return 1
+
+
 
     def insert_data(self, fname):
         self.list.delete(*self.list.get_children())
         with open(fname, newline="") as f:
             for contact in f:
-                contact = contact.split()[0] + ' ' + contact.split()[1] + ' ' + self.new_key.decrypt_func(contact.split()[2])
+                contact = contact.split()[0] + ' ' + contact.split()[1] + ' ' + '*' * len(self.new_key.decrypt_func(contact.split()[2]))
                 self.list.insert("", tk.END, values = contact)
 
     def add_func(self, fname):
@@ -134,8 +205,6 @@ class New_tab():
         entery.delete(0, tk.END)
         entery.insert(0, password)
 
-
-
     def commit_func(self, entery1, entery2, entery3, fname):
         info1 = entery1.get()
         info2 = entery2.get()
@@ -151,6 +220,67 @@ class New_tab():
             delete_from_file(fname, self.list.item(selection)['values'])
             self.list.delete(selection)
 
+    def show_info(self, fname):
+        selection = self.list.selection()[0]
+        f = open(fname)
+        #print(self.list.item(selection)['values'])
+        selected_line = self.list.item(selection)['values']
+        temp = ''
+        for line in f:
+            if line.split()[0] == selected_line[0] and str(line.split()[1]) == str(selected_line[1]):
+                temp = line
+        f.close()
+
+        print(temp)
+
+        show_window = tk.Tk()
+        show_window.title(selected_line[0])
+
+        show_frame = tk.Frame(show_window)
+        show_frame.pack(expand=True)
+
+        label2 = tk.Label(show_frame, text="Логин:")
+        label2.grid(row=0, column=0)
+
+        login_entry = tk.Entry(show_frame)
+        login_entry.insert(0, temp.split()[1])
+        login_entry.grid(row=0, column=1)
+        login_entry['state'] = 'disabled'
+
+        button_copy1 = tk.Button(show_frame,
+                                 text='Копировать',
+                                 command = lambda: [show_window.clipboard_clear(),
+                                                                             show_window.clipboard_append(login_entry.get().rstrip()),
+                                                                             label4.config(text="Логин скопирован!")])
+        button_copy1.grid(row=0,column=2)
+
+
+        label3 = tk.Label(show_frame, text="Пароль:")
+        label3.grid(row=1, column=0)
+
+        password_entry = tk.Entry(show_frame)
+        password_entry.insert(0, self.new_key.decrypt_func(temp.split()[2]))
+        password_entry.grid(row=1, column=1)
+        password_entry['state'] = 'disabled'
+
+        button_copy2 = tk.Button(show_frame,
+                                 text='Копировать',
+                                 command=lambda: [show_window.clipboard_clear(),
+                                                  show_window.clipboard_append(password_entry.get().rstrip()),
+                                                  label4.config(text="Пароль скопирован!")])
+        button_copy2.grid(row=1, column=2)
+
+        label4 = tk.Label(show_frame)
+        label4.grid(row=2, column=1)
+
+        window_width = 260
+        window_height = 260
+
+        x = (screen_width // 2) - (window_width // 2)
+        y = (screen_height // 2) - (window_height // 2)
+
+        show_window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+        show_window.resizable(width=False, height=False)
 
 root = tk.Tk()
 root.resizable(width=False, height=False)
